d9da8566260b91b9dd6f483a5260109e2607fc05
Fiona Gruell <Fiona@pc09.pool.tp1.uni-duesseldorf.de>
2015-10-28 11:02:57 +0100

    pogram by fionagruell

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,6 +1,22 @@
 #include <iostream>
-
+using namespace std;
 int main(){
-  std::cout << "hallo" << std::endl;
+  int N;
+    cout << "Bitte Zahl N eingeben!" << endl;
+    cin >> N;
+    cout << "Danke. Sie haben " << N << " eingegeben" << endl;
+
+ int f0=0;
+ int f1=1;
+ int fi ; 
+  for(int i=0; i<N+1; i++){
+    cout << "Die Summe der Fibonaccifolge ist  für i="<<i <<" " <<f0 << endl;
+  fi = f0+f1;
+  f0 = f1;
+  f1 = fi;
+  
+  
+   
+  }
   return 0;
 }
